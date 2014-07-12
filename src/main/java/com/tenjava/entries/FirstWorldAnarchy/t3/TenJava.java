acc691382d39c3fdbacd747fd04683d1292e9a49
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 12:09:12 -0400

    Changed some things

--- src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
@@ -3,13 +3,16 @@ package com.tenjava.entries.FirstWorldAnarchy.t3;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.EventHandlers;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.RandomEvent;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.Storms;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class TenJava extends JavaPlugin {
 
     private static TenJava instance = new TenJava();
-    
+
     private class RandomEventChecker extends BukkitRunnable {
 
         @Override
@@ -19,18 +22,34 @@ public class TenJava extends JavaPlugin {
                 getServer().getPluginManager().callEvent(new RandomEvent());
             }
         }
-        
     }
-    
+
+    private class DamagePlayerFromStorm extends BukkitRunnable {
+        @Override
+        public void run() {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (Storms.isStormInProgress()) {
+                    if (Storms.getCurrentStorm() == Storms.ACID_RAIN) {
+                        if (player.getWorld().getHighestBlockAt(player.getLocation()).getType() == Material.AIR) {
+                            player.damage(0.2);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new EventHandlers(), this);
         new RandomEventChecker().runTaskTimer(this, 0, getConfig().getInt("random_delay"));
+        new DamagePlayerFromStorm().runTaskTimer(this, 0, 20);
     }
 
     public static TenJava getInstance() {
         return instance;
     }
-    
+
 }
