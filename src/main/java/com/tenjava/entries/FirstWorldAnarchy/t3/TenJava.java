4dd55ba7678b873ea0da87659a4c216212cac241
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 10:13:08 -0400

    Added on enable method, implemented Listener to new EventHandlers class.

--- src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
@@ -2,4 +2,12 @@ package com.tenjava.entries.FirstWorldAnarchy.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    
+    
+}
