d04703bbd73a03f8b3e71da8af12150bed2a963a
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 10:50:27 -0400

    Template class for initializing all storms

---
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/Storms.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tenjava.entries.FirstWorldAnarchy.t3.events;
+
+/**
+ *
+ * @author family
+ */
+public class Storms {
+    private static Storms instance;
+    
+    public static Storms getInstance() {
+        return instance;
+    }
+    
+    public void startMeteorShower() {
+        
+    }
+    
+    public void startAcidRain() {
+        
+    }
+    
+    public void startTornados() {
+        
+    }
+    
+    
+}
