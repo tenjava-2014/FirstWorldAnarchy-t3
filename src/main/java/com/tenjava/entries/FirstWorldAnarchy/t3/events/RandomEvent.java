2381120800277f28579d0e548250f14ae8050caa
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 10:26:48 -0400

    Random events, because bukkit is too stupid to check for random game ticks!

---
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/RandomEvent.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tenjava.entries.FirstWorldAnarchy.t3.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ *
+ * @author family
+ */
+public class RandomEvent extends Event {
+
+    private static HandlerList handlers;
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+}
