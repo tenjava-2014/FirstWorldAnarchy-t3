7d6ec47626adc9437ea855fa6a6fcbe26235d301
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 11:33:58 -0400

    Starting to add the mechanics of starting a storm!

--- src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/Storms.java
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/Storms.java
@@ -3,40 +3,71 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.tenjava.entries.FirstWorldAnarchy.t3.events;
+
+import com.tenjava.entries.FirstWorldAnarchy.t3.TenJava;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 /**
  *
  * @author family
  */
 public enum Storms {
+
     METEOR_SHOWER("storms.meteor_shower"), ACID_RAIN("storms.acid_rain"), TORNADOS("storms.tornados"), EARTHQUAKES("storms.earthquakes");
-    
+
     private String alias;
     
+    private static boolean stormInProgress;
+
+    public static boolean isStormInProgress() {
+        return stormInProgress;
+    }
+    
+    private static void setStormInProgress(boolean stormInProgress) {
+        Storms.stormInProgress = stormInProgress;
+    }
+
     private Storms(String alias) {
         this.alias = alias;
     }
-    
+
     public String getAlias() {
         return alias;
     }
-    
+
     public static void startMeteorShower() {
-        
+        Bukkit.broadcastMessage(ChatColor.DARK_RED + "A meteor shower has started! Take cover!");
+        setStormInProgress(true);
     }
-    
+
     public static void startAcidRain() {
-        
+        Bukkit.broadcastMessage(ChatColor.BLUE + "It's raining acid! Take cover!");
+        setStormInProgress(true);
     }
-    
+
     public static void startTornado() {
-        
+        Bukkit.broadcastMessage(ChatColor.GRAY + "A tornado has spawned! Take cover!");
+        setStormInProgress(true);
     }
-    
+
     public static void startEarthquake() {
-        
+        Bukkit.broadcastMessage(ChatColor.DARK_GREEN + "An earthquake has started! Take cover!");
+        setStormInProgress(true);
     }
-    
+
+    public static void startCorrespondingStorm(String configName) {
+        if (configName.equalsIgnoreCase(METEOR_SHOWER.getAlias()) && TenJava.getInstance().getConfig().getBoolean(METEOR_SHOWER.getAlias())) {
+            startMeteorShower();
+        } else if (configName.equalsIgnoreCase(TORNADOS.getAlias()) && TenJava.getInstance().getConfig().getBoolean(TORNADOS.getAlias())) {
+            startTornado();
+        } else if (configName.equalsIgnoreCase(ACID_RAIN.getAlias()) && TenJava.getInstance().getConfig().getBoolean(ACID_RAIN.getAlias())) {
+            startAcidRain();
+        } else if (configName.equalsIgnoreCase(EARTHQUAKES.getAlias()) && TenJava.getInstance().getConfig().getBoolean(EARTHQUAKES.getAlias())) {
+            startEarthquake();
+        } else {
+            startMeteorShower();
+        }
+    }
 }
