cf702dccec8285df2c218acef4ec34e09519e836
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 15:03:28 -0400

    More changes to acid rain

--- src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/TenJava.java
@@ -3,8 +3,11 @@ package com.tenjava.entries.FirstWorldAnarchy.t3;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.EventHandlers;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.RandomEvent;
 import com.tenjava.entries.FirstWorldAnarchy.t3.events.Storms;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -25,15 +28,18 @@ public class TenJava extends JavaPlugin {
     }
 
     private class DamagePlayerFromStorm extends BukkitRunnable {
+
         @Override
         public void run() {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 if (Storms.isStormInProgress()) {
                     if (Storms.getCurrentStorm() == Storms.ACID_RAIN) {
+                        if (playerInRain(player)) {
                             if (player.getHealth() > 0d) {
                                 player.damage(0.2);
                                 player.setHealth(player.getHealth() - 0.2);
                             }
+                        }
                     }
                 }
             }
@@ -46,7 +52,7 @@ public class TenJava extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new EventHandlers(), this);
-        new RandomEventChecker().runTaskTimer(this, 0, getConfig().getInt("random_delay") * 1200);
+        new RandomEventChecker().runTaskTimer(this, 0, getConfig().getInt("random_interval") * 1200);
         new DamagePlayerFromStorm().runTaskTimer(this, 0, 20);
     }
 
@@ -54,10 +60,16 @@ public class TenJava extends JavaPlugin {
         return instance;
     }
 
-    public void checkIsRainingOn(Player p) {
-        if (p.getWorld().hasStorm()) {
-            if ()
+    public boolean playerInRain(Player p) {
+        Location location = p.getEyeLocation();
+        List<Integer> materials = new ArrayList<Integer>();
+        for (int i = location.getBlockY() + 1; i <= location.getWorld().getMaxHeight(); i++) {
+            materials.add(location.getBlock().getType() == Material.AIR ? 0 : 1);
         }
+        if (p.getWorld().hasStorm() && materials.contains(1)) return false;
+        if (p.getWorld().hasStorm() && !materials.contains(1)) return true;
+        if (!p.getWorld().hasStorm()) return false;
+        return false;
     }
-    
+
 }
