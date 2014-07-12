f536f7723a688d0fbef48cccbaa2db5a8ccbb860
FirstWorldAnarchy <tarunbgamer@gmail.com>
2014-07-12 11:34:14 -0400

    We're now able to start storms!

--- src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/EventHandlers.java
+++ src/main/java/com/tenjava/entries/FirstWorldAnarchy/t3/events/EventHandlers.java
@@ -20,25 +20,23 @@ public class EventHandlers implements Listener {
     @EventHandler
     public void onRandomEvent(RandomEvent e) {
         int random = (int) Math.floor(Math.random() * 4 + 1);
-        Storms storm;
+        Storms storm = Storms.METEOR_SHOWER;
         switch (random) {
             case 1:
                 storm = Storms.METEOR_SHOWER;
-                Storms.startMeteorShower();
                 break;
             case 2:
                 storm = Storms.TORNADOS;
-                Storms.startTornado();
                 break;
             case 3:
                 storm = Storms.ACID_RAIN;
-                Storms.startAcidRain();
                break;
             case 4:
                 storm = Storms.EARTHQUAKES;
-                Storms.startEarthquake();
                 break;
         }
+        
+        Storms.startCorrespondingStorm(storm.getAlias());
         
     }
 }
